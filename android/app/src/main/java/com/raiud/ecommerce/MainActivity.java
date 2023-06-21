9cd22106dee559829141da83472494686327b477
dsicped3 <dsicped3@alamana.org.ma>
2023-06-21 18:22:38 +0000

    android configuration

---
+++ android/app/src/main/java/com/raiud/ecommerce/MainActivity.java
@@ -0,0 +1,5 @@
+package com.raiud.ecommerce;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.raiud.ecommerce',
   appName: 'raiud-ecommerce',
   webDir: 'www',
   server: {
