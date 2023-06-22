6c9a1a64a65a9c4791ab2f5bfa09b068b4020cc4
dsicped3 <dsicped3@alamana.org.ma>
2023-06-22 07:11:50 +0000

    installed some plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,62 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { JWT_OPTIONS, JwtModule } from '@auth0/angular-jwt';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { TokeninterseptService } from './interseptors/tokenintersept.service';
+import { IonicStorageModule, Storage } from '@ionic/storage-angular';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { StatusBar } from '@capacitor/status-bar';
+import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Printer } from '@awesome-cordova-plugins/printer/ngx';
+import { LaunchNavigator } from '@awesome-cordova-plugins/launch-navigator/ngx';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { Geolocation } from '@capacitor/geolocation';
+export function jwtOptionsFactory(storage:any) {
+  return {
+    tokenGetter: () => {
+      return storage.get('token');
+    }
+  }
+}
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicStorageModule.forRoot(), JwtModule.forRoot({
+    jwtOptionsProvider: {
+      provide: JWT_OPTIONS,
+      useFactory: jwtOptionsFactory,
+      deps: [Storage]
+    }
+  }), IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, FormsModule, FontAwesomeModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  registrationStrategy: 'registerWhenStable:30000'
+})],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: TokeninterseptService, multi: true },
+    CallNumber,
+    Printer,
+    LaunchNavigator
+  ],
   bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {}
+
+export class AppModule {
+
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, fab, far);
+  }
+
+}
